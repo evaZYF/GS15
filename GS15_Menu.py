52fa846eb5fc736b8e99963b36ea6841d5da767b
jonathan.bernales <jonathan.bernales@jc-utt.fr>
2018-01-02 17:32:03 +0100

    génération clé

--- GS15_Menu.py
+++ GS15_Menu.py
@@ -1,13 +1,15 @@
-
 # coding: utf-8
 import os
 import sys
+import random
+
+
 def print_menu():
     print ""
     print "Bienvenue ! Vous venez d'entrer dans un nouveau monde, celui de la cryptographie ! 'whouaaa'"
     print "Ce petit script vous permettra d'utiliser 2 chiffrements et une fonction de hashage."
     print "Son fonctionnement est très simple... Dans le menu suivant, il vous suffit de choisir la fonction désirée et de se laisser guider... Bon voyage !\n"
-    print "<"+5 * "-="+"GS15 PROJECT / MENU"+5 * "=-"+">"
+    print "<" + 5 * "-=" + "GS15 PROJECT / MENU" + 5 * "=-" + ">"
     print "->1<- Chiffrement symétrique ThreeFish"
     print "->2<- Chiffrement de Cramer-Shoup"
     print "->3<- Hash d'un message"
@@ -15,14 +17,15 @@ def print_menu():
     print "->5<- Déchiffrement de Cramer-Shoup"
     print "->6<- Vérification d'un hash"
     print "->7<- Exit..."
-    print "<-="+9 * "-="+9 * "=-"+"=->"
+    print "<-=" + 9 * "-=" + 9 * "=-" + "=->"
+
 
 loop = True
 while loop:
     print_menu()
     choix = input("\nSaisir le numéro de la fonction choisie [1-7] : ")
 
-    if choix==1:
+    if choix == 1:
         print " -> Chiffrement symétrique ThreeFish choisi, suivez le guide ><(((º> ..."
         print "Pour effectuer le chiffrement, il nous faut une taille de blocs pour effectuer ce chiffrement"
         taille = True
@@ -35,75 +38,105 @@ while loop:
                 print "512 blocs choisis"
                 taille = False
             elif tailleBlocs == 1024:
-                print("1024 bits choisis pour ")
-                tailleBlocs = False
+                print("1024 bits choisis")
+                taille = False
             else:
                 print "Il semblerait que le choix entre 256, 512 ou 1024 ait été trop compliqué, veuillez réessayer"
-        print "Vous avez choisi des blocs de"+str(tailleBlocs)+" bits"
 
-        myList = []
+        print "Vous avez choisi des blocs dev" + str(tailleBlocs) + " bits"
+
+        C = "0001101111010001000110111101101010101001111111000001101000100010"
+        t0 = ""
+        t1 = ""
+        t2 = ""
+        for i in range(0, 64, 1):
+            a = random.randint(0,1)
+            t0 = str(t0)+str(a)
+            b = random.randint(0,1)
+            t1 = str(t1)+str(b)
+        print "===================="
+        print("T0 = " + str(t0))
+        print("T1 = " + str(t1))
+        t2 = int(t0, 2) ^ int(t1, 2)
+        print "T2 = " + str(bin(t2)[2:].zfill(len(t0)))
+        cleK = []
+        for i in range(0, tailleBlocs/64, 1):
+            a = random.randint(0, 1)
+            cleK.append(str(a))
+            for j in range(0, 63, 1):
+                a = random.randint(0, 1)
+                cleK[i] = str(cleK[i]) + str(a)
+        kN = "0"
+        for i in range(len(cleK)):
+            kN = int(str(kN), 10) ^ int(cleK[i], 2)
+        kN = int(str(kN), 10) ^ int(C, 2)
+        print("k"+str(tailleBlocs/64)+" = " + str(bin(kN)[2:].zfill(64)))
+
+        print "Clé générée : "
+        for i in range(len(cleK)):
+            #print cleK[i]
+            sys.stdout.write(cleK[i])
+        print("")
+
+        cleK.append(bin(kN)[2:].zfill(64))
+
+        print("Clé finale (A CONSERVER) : ")
+        for i in range(len(cleK)):
+            #print cleK[i]
+            sys.stdout.write(cleK[i])
+        print ""
+        print("====================")
+
+
+#Lecture et traitement du fichier ================================================================
+        motsFichier = []
         with open(os.path.expanduser("~/Desktop/myfile.rtf"), "rb") as file:
-            byte = file.read(1)
             compteur = 0
-            compteurComplet = 0
-            while byte != "":
-                # Do stuff with byte.
+            while True:
                 byte = file.read(1)
-#Encodage en hexadecimal=========================
-                #print byte.encode('hex'),
-                #print ", ",
-#Encodage en hexadecimal=========================
-
-                #Encodage en binaire
+                if byte.encode('hex') == '':
+                    break
                 if byte.encode('hex') != '':
                     binaire = bin(int(byte.encode('hex'), 16))[2:].zfill(8)
-                    #print "Binaire = " + str(binaire)
-                    #print "Compteur = " + str(compteur)
+                    # print "Binaire = " + str(binaire)
+                    # print "Compteur = " + str(compteur)
                     if compteur != 0:
-                        if len(str(myList[compteur-1])) == 64:
-                            #print "Longueur : " + str(myList[compteur-1])
-                            myList.append(str(binaire))
-                            compteur +=1
+                        if len(str(motsFichier[compteur - 1])) == 64:
+                            # print "Longueur : " + str(myList[compteur-1])
+                            motsFichier.append(str(binaire))
+                            compteur += 1
                         else:
-                            myList[compteur-1] = str(myList[compteur-1])+str(binaire)
+                            motsFichier[compteur - 1] = str(motsFichier[compteur - 1]) + str(binaire)
                     else:
-                        myList.insert(compteur, binaire)
-                        #print "ELSE : "+str(myList[compteur])
+                        motsFichier.insert(compteur, binaire)
+                        # print "ELSE : "+str(myList[compteur])
                         compteur += 1
-#Test lecture fichier=============================
-                #print binaire
-                #print "  |  ",
-                sys.stdout.write(binaire)
-#Test lecture fichier=============================
-        print "================"
-        for i in range(len(myList)):
-            #print myList[i],
-            sys.stdout.write(myList[i])
+                    #sys.stdout.write(binaire)
 
-    elif choix==2:
+        # Test lecture fichier=============================
+        #print ""
+        for i in range(len(motsFichier)):
+            print motsFichier[i]
+            #sys.stdout.write(motsFichier[i])
+
+    elif choix == 2:
         print " -> Chiffrement de Cramer-Shoup choisi, ..."
         ## fonction Cramer-Shoup en mode chiffrement
-    elif choix==3:
+    elif choix == 3:
         print " -> Hash d'un message choisi, ..."
         ## fonction da hashage
-    elif choix==4:
+    elif choix == 4:
         " -> Déchiffrement symétrique ThreeFish choisi, le guide revient sur ses pas <(((>< ..."
         ## fonction ThreeFish en mode déchiffrement
-    elif choix==5:
+    elif choix == 5:
         print " -> Déchiffrement de Cramer-Shoup choisi, ..."
         ## fonction Cramer-Shoup en mode déchiffrement
-    elif choix==6:
+    elif choix == 6:
         print " -> Vérification d'un hash choisi, ..."
         ## fonction de hashage
-    elif choix==7:
+    elif choix == 7:
         print " Vous avez choisi de partir, c'est bien dommage..."
-        loop=False
+        loop = False
     else:
-        raw_input("---ERROR--ERROR--ERROR---\nCette option n'existe pas, veuillez appuyer sur une touche pour continuer...")
-
-
-
-
-
-
-
+        raw_input(
+            "---ERROR--ERROR--ERROR---\nCette option n'existe pas, veuillez appuyer sur une touche pour continuer...")
